00083fa91c6cc4923037233328c0eb018db8af2c
Sayan Nandi <sayannandi11071998@gmail.com>
2025-03-23 01:50:04 +0530

    Health and Damage Added

--- Assets/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Scripts/Enemy/EnemySpawner.cs
@@ -6,6 +6,7 @@ public class EnemySpawner : MonoBehaviour
 {
     [SerializeField] private List<WaveConfigSO> waveConfigs;
     [SerializeField] private float timeBetweenWaves = 0f;
+    [SerializeField] private bool isLooping;
 
     private WaveConfigSO currentWave;
 
@@ -16,25 +17,28 @@ public class EnemySpawner : MonoBehaviour
 
     private IEnumerator SpawnEnemyWaves()
     {
-        foreach (WaveConfigSO wave in waveConfigs)
+        do
         {
-            currentWave = wave;
-
-            for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+            foreach (WaveConfigSO wave in waveConfigs)
             {
-                Instantiate
-                (
-                    currentWave.GetEnemyPrefab(i),
-                    currentWave.GetStartingWaypoint().position,
-                    Quaternion.identity,
-                    transform
-                );
+                currentWave = wave;
 
-                yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                for (int i = 0; i < currentWave.GetEnemyCount(); i++)
+                {
+                    Instantiate
+                    (
+                        currentWave.GetEnemyPrefab(i),
+                        currentWave.GetStartingWaypoint().position,
+                        Quaternion.identity,
+                        transform
+                    );
+
+                    yield return new WaitForSeconds(currentWave.GetRandomSpawnTime());
+                }
+
+                yield return new WaitForSeconds(timeBetweenWaves);
             }
-
-            yield return new WaitForSeconds(timeBetweenWaves);
-        }
+        } while (isLooping);
     }
 
     public WaveConfigSO GetCurrentWave() => currentWave;
